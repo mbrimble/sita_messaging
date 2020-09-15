6e22958c9e044fc2db204c4259e2e8caf254cea2
Mark Brimble <mbrimble@sc-che-366047.UoA.auckland.ac.nz>
2020-09-16 09:26:02 +1200

    Added SITA schema

---
+++ BT.SITAMessaging/RunMe.bat
@@ -0,0 +1,5 @@
+xcopy ExternalSchemas.btproj.user "D:\Temp\AIA.BT.SITAMessaging\AIA.BT.SITAMessaging\ExternalSchemas\" /Y /I
+xcopy InternalSchemas.btproj.user "D:\Temp\AIA.BT.SITAMessaging\AIA.BT.SITAMessaging\InternalSchemas\" /Y /I
+xcopy Maps.btproj.user "D:\Temp\AIA.BT.SITAMessaging\AIA.BT.SITAMessaging\Maps\" /Y /I
+xcopy Orchestrations.btproj.user "D:\Temp\AIA.BT.SITAMessaging\AIA.BT.SITAMessaging\Orchestrations\" /Y /I
+xcopy Pipelines.btproj.user "D:\Temp\AIA.BT.SITAMessaging\AIA.BT.SITAMessaging\Pipelines\" /Y /I
---
+++ BT.SITAMessaging/UnitTests/ExternalFFSchemasUnitTest.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BT.SITAMessaging.ExternalSchemas;
+
+namespace ExternalFFSchemaUnitTests
+{
+    [TestClass]
+    public class ExternalFFSchemasUnitTest
+    {
+        [TestMethod]
+        public void TestUC_MVT001()
+        {
+            AircraftMovementMessageTypeBFF movement = new AircraftMovementMessageTypeBFF();
+
+            //path to FF instances to test - relative to the test dir
+            string validFF = @"..\..\TestData\External\TypeBMsg\UC_MVT001.RCV";
+
+            //Validating the valid instance against the schema
+            Assert.IsTrue(movement.ValidateInstance(validFF, Microsoft.BizTalk.TestTools.Schema.OutputInstanceType.Native));
+        }
+        [TestMethod]
+        public void TestUC_MVT002()
+        {
+            AircraftMovementMessageTypeBFF movement = new AircraftMovementMessageTypeBFF();
+
+            //path to FF instances to test - relative to the test dir
+            string validFF = @"..\..\TestData\External\TypeBMsg\UC_MVT002.RCV";
+
+            //Validating the valid instance against the schema
+            Assert.IsTrue(movement.ValidateInstance(validFF, Microsoft.BizTalk.TestTools.Schema.OutputInstanceType.Native));
+        }
+        [TestMethod]
+        public void TestUC_MVT003()
+        {
+            AircraftMovementMessageTypeBFF movement = new AircraftMovementMessageTypeBFF();
+
+            //path to FF instances to test - relative to the test dir
+            string validFF = @"..\..\TestData\External\TypeBMsg\UC_MVT003.RCV";
+
+            //Validating the valid instance against the schema
+            Assert.IsTrue(movement.ValidateInstance(validFF, Microsoft.BizTalk.TestTools.Schema.OutputInstanceType.Native));
+        }
+        [TestMethod]
+        public void TestUC_MVT004()
+        {
+            AircraftMovementMessageTypeBFF movement = new AircraftMovementMessageTypeBFF();
+
+            //path to FF instances to test - relative to the test dir
+            string validFF = @"..\..\TestData\External\TypeBMsg\UC_MVT004.RCV";
+
+            //Validating the valid instance against the schema
+            Assert.IsTrue(movement.ValidateInstance(validFF, Microsoft.BizTalk.TestTools.Schema.OutputInstanceType.Native));
+        }
+        [TestMethod]
+        public void TestUC_MVT005()
+        {
+            AircraftMovementMessageTypeBFF movement = new AircraftMovementMessageTypeBFF();
+
+            //path to FF instances to test - relative to the test dir
+            string validFF = @"..\..\TestData\External\TypeBMsg\UC_MVT005.RCV";
+
+            //Validating the valid instance against the schema
+            Assert.IsTrue(movement.ValidateInstance(validFF, Microsoft.BizTalk.TestTools.Schema.OutputInstanceType.Native));
+        }
+        [TestMethod]
+        public void TestUC_MVT006()
+        {
+            AircraftMovementMessageTypeBFF movement = new AircraftMovementMessageTypeBFF();
+
+            //path to FF instances to test - relative to the test dir
+            string validFF = @"..\..\TestData\External\TypeBMsg\UC_MVT006.RCV";
+
+            //Validating the valid instance against the schema
+            Assert.IsTrue(movement.ValidateInstance(validFF, Microsoft.BizTalk.TestTools.Schema.OutputInstanceType.Native));
+        }
+        [TestMethod]
+        public void TestUC_MVT007()
+        {
+            AircraftMovementMessageTypeBFF movement = new AircraftMovementMessageTypeBFF();
+
+            //path to FF instances to test - relative to the test dir
+            string validFF = @"..\..\TestData\External\TypeBMsg\UC_MVT007.RCV";
+
+            //Validating the valid instance against the schema
+            Assert.IsTrue(movement.ValidateInstance(validFF, Microsoft.BizTalk.TestTools.Schema.OutputInstanceType.Native));
+        }
+        [TestMethod]
+        public void TestUC_MVT008()
+        {
+            AircraftMovementMessageTypeBFF movement = new AircraftMovementMessageTypeBFF();
+
+            //path to FF instances to test - relative to the test dir
+            string validFF = @"..\..\TestData\External\TypeBMsg\UC_MVT008.RCV";
+
+            //Validating the valid instance against the schema
+            Assert.IsTrue(movement.ValidateInstance(validFF, Microsoft.BizTalk.TestTools.Schema.OutputInstanceType.Native));
+        }
+        [TestMethod]
+        public void TestUC_MVT009()
+        {
+            AircraftMovementMessageTypeBFF movement = new AircraftMovementMessageTypeBFF();
+
+            //path to FF instances to test - relative to the test dir
+            string validFF = @"..\..\TestData\External\TypeBMsg\UC_MVT009.RCV";
+
+            //Validating the valid instance against the schema
+            Assert.IsTrue(movement.ValidateInstance(validFF, Microsoft.BizTalk.TestTools.Schema.OutputInstanceType.Native));
+        }
+        [TestMethod]
+        public void TestUC_MVT010()
+        {
+            AircraftMovementMessageTypeBFF movement = new AircraftMovementMessageTypeBFF();
+
+            //path to FF instances to test - relative to the test dir
+            string validFF = @"..\..\TestData\External\TypeBMsg\UC_MVT010.RCV";
+
+            //Validating the valid instance against the schema
+            Assert.IsTrue(movement.ValidateInstance(validFF, Microsoft.BizTalk.TestTools.Schema.OutputInstanceType.Native));
+        }
+        [TestMethod]
+        public void TestUC_MVT011()
+        {
+            AircraftMovementMessageTypeBFF movement = new AircraftMovementMessageTypeBFF();
+
+            //path to FF instances to test - relative to the test dir
+            string validFF = @"..\..\TestData\External\TypeBMsg\UC_MVT011.RCV";
+
+            //Validating the valid instance against the schema
+            Assert.IsTrue(movement.ValidateInstance(validFF, Microsoft.BizTalk.TestTools.Schema.OutputInstanceType.Native));
+        }
+        [TestMethod]
+        public void TestUC_MVT012()
+        {
+            AircraftMovementMessageTypeBFF movement = new AircraftMovementMessageTypeBFF();
+
+            //path to FF instances to test - relative to the test dir
+            string validFF = @"..\..\TestData\External\TypeBMsg\UC_MVT012.RCV";
+
+            //Validating the valid instance against the schema
+            Assert.IsTrue(movement.ValidateInstance(validFF, Microsoft.BizTalk.TestTools.Schema.OutputInstanceType.Native));
+        }
+        [TestMethod]
+        public void TestUC_MVT013()
+        {
+            AircraftMovementMessageTypeBFF movement = new AircraftMovementMessageTypeBFF();
+
+            //path to FF instances to test - relative to the test dir
+            string validFF = @"..\..\TestData\External\TypeBMsg\UC_MVT013.RCV";
+
+            //Validating the valid instance against the schema
+            Assert.IsTrue(movement.ValidateInstance(validFF, Microsoft.BizTalk.TestTools.Schema.OutputInstanceType.Native));
+        }
+        [TestMethod]
+        public void TestUC_MVT014()
+        {
+            AircraftMovementMessageTypeBFF movement = new AircraftMovementMessageTypeBFF();
+
+            //path to FF instances to test - relative to the test dir
+            string validFF = @"..\..\TestData\External\TypeBMsg\UC_MVT014.RCV";
+
+            //Validating the valid instance against the schema
+            Assert.IsTrue(movement.ValidateInstance(validFF, Microsoft.BizTalk.TestTools.Schema.OutputInstanceType.Native));
+        }
+        [TestMethod]
+        public void TestUC_MVT015()
+        {
+            AircraftMovementMessageTypeBFF movement = new AircraftMovementMessageTypeBFF();
+
+            //path to FF instances to test - relative to the test dir
+            string validFF = @"..\..\TestData\External\TypeBMsg\UC_MVT015.RCV";
+
+            //Validating the valid instance against the schema
+            Assert.IsTrue(movement.ValidateInstance(validFF, Microsoft.BizTalk.TestTools.Schema.OutputInstanceType.Native));
+        }
+    }
+}
